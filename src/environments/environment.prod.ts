4edca758a7590acb5075763f697f5bf4961aecde
ayeshaazhari <aisha.siddiqua77@gmail.com>
2019-12-03 12:46:03 +0530

    production environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseconfig :  {
+    apiKey: "AIzaSyDtYlSHu7xgeLziRuVCbDZCVmeN_o6N2TY",
+    authDomain: "socialaccount-7a1be.firebaseapp.com",
+    databaseURL: "https://socialaccount-7a1be.firebaseio.com",
+    projectId: "socialaccount-7a1be",
+    storageBucket: "socialaccount-7a1be.appspot.com",
+    messagingSenderId: "649745620126",
+    appId: "1:649745620126:web:c7e17c26c3b1edf7792f8f",
+    measurementId: "G-RSHCNHVR5J"
+    }
 };
